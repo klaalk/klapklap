ebe006c46b695c074a6eb57298c4e4ae2020cdea
Michele Luigi Greco <grecomichele96@gmail.com>
2019-05-01 18:02:33 +0200

    creato progetto server

---
+++ server/src/PDS_PRG_SERVER/PDS_PRG_SERVER/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PDS_PRG_SERVER
+//
+//  Created by Michele Luigi Greco on 01/05/2019.
+//  Copyright © 2019 Michele Luigi Greco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
