3d7a63adf488121126750dcf2a6ba26bd994a328
Michele Luigi Greco <grecomichele96@gmail.com>
2019-05-02 12:22:26 +0200

    Fixed Boost.Asio problem. Fixed cMakeLists.

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -7,6 +7,10 @@
 //
 
 #include <iostream>
+#include <boost/chrono.hpp>
+#include <boost/asio.hpp>
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
