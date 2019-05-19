ff1a4a62eeb490a979537d95cba8aab9b3c1dc99
Michele <michele.greco@polito.it>
2019-05-19 18:43:43 +0200

    working only crdt copy

--- server/src/classes/crdt_rc/CRDT_main.cpp
+++ server/src/classes/crdt_rc/CRDT_main.cpp
@@ -4,28 +4,28 @@
 #include <stdlib.h>
 #include <string>
 
-int main(){
-
-   /* Test compare To
-    * int i;
-    identifier *a = new identifier(1,"polito");
-    identifier *b = new identifier(2,"polito");
-    //std::string siteid= "polito";
-    CRDT_Char *A = new CRDT_Char('A',"polito");
-    CRDT_Char *B= new CRDT_Char('A',"polito");
-
-    A->push_identifier(*a);
-    B->push_identifier(*b);
-
-
-    i=A->compareTo(*B);
-    std::cout<<i;
-*/
-
-
-
-
-
-
-    return  0;
-}
+//int main(){
+//
+//   /* Test compare To
+//    * int i;
+//    identifier *a = new identifier(1,"polito");
+//    identifier *b = new identifier(2,"polito");
+//    //std::string siteid= "polito";
+//    CRDT_Char *A = new CRDT_Char('A',"polito");
+//    CRDT_Char *B= new CRDT_Char('A',"polito");
+//
+//    A->push_identifier(*a);
+//    B->push_identifier(*b);
+//
+//
+//    i=A->compareTo(*B);
+//    std::cout<<i;
+//*/
+//
+//
+//
+//
+//
+//
+//    return  0;
+//}
