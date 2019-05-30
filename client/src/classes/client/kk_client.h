b89f40e294a909d17689645bc58054612284082e
Klaus Cuko <klaus.cuko@gmail.com>
2019-05-30 21:26:53 +0200

    setup client for qt creator

--- client/src/classes/client/kk_client.h
+++ client/src/classes/client/kk_client.h
@@ -9,69 +9,35 @@
 #include <cstdlib>
 #include <deque>
 #include <iostream>
-#include <boost/bind.hpp>
-#include <boost/asio.hpp>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 
+#include <QtCore/QObject>
+#include <QtWebSockets/QWebSocket>
+#include <QtNetwork/QSslError>
+#include <QtCore/QList>
+#include <QtCore/QString>
+#include <QtCore/QUrl>
+
 #include "../../../../libs/src/classes/payload/kk_payload.h"
 #include "../../../../libs/src/constants/kk_constants.h"
 
-using boost::asio::ip::tcp;
-typedef std::deque<kk_payload> kk_payload_queue;
+QT_FORWARD_DECLARE_CLASS(QWebSocket)
 
-class kk_client
+class kk_client : public QObject
 {
+    Q_OBJECT
 public:
-    kk_client(boost::asio::io_service& io_service,
-                tcp::resolver::iterator endpoint_iterator);
+    explicit kk_client(const QUrl &url, QObject *parent = nullptr);
 
-    kk_client_connection_state get_connection_state();
-    kk_client_state get_state();
-
-    void send_login();
-    void send_signup();
-    void send_openfile();
-    void send_chat();
-    void send_crdt();
-
-    void menu();
-    void close();
-
+private Q_SLOTS:
+    void onConnected();
+    void onMessageReceived(QString message);
+    void onSslErrors(const QList<QSslError> &errors);
+    void closeConnection();
 private:
-
-    void send_kk_payload(const char *line, kk_payload_type _type);
-    void receive_kk_payload();
-
-    void do_connection(tcp::resolver::iterator endpoint_iterator);
-    void handle_connect(const boost::system::error_code& error,
-                        tcp::resolver::iterator endpoint_iterator);
-
-    void do_read();
-    void handle_read_header(const boost::system::error_code& error);
-    void handle_read_body(const boost::system::error_code& error);
-
-    void do_write(kk_payload msg);
-    void handle_write(const boost::system::error_code& error);
-
-    void do_close();
-    void print_chat();
-
-
-    boost::asio::io_service& io_service_;
-    tcp::socket socket_;
-
-    kk_payload read_msg_;
-    kk_payload_queue write_msgs_;
-
-    kk_client_connection_state connection_state;
-    kk_client_state state;
-
-    std::condition_variable cv;
-    std::mutex cv_m;
-
-    char username[128], pass[128];
+    QWebSocket m_webSocket;
 };
 
 
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,8 +1,6 @@
 #include <cstdlib>
 #include <deque>
 #include <iostream>
-#include <boost/bind.hpp>
-#include <boost/asio.hpp>
 #include <pthread.h>
 
 
@@ -13,40 +11,18 @@
 #include <QtWidgets/QMainWindow>
 
 
-#include "../../libs/src/classes/payload/kk_payload.h"
 #include "classes/client/kk_client.h"
+#include "classes/textedit/textedit.h"
 
-//#include "classes/textedit/textedit.h"
-
-boost::asio::io_service io_service;
-using boost::asio::ip::tcp;
 
 
 int main(int argc, char* argv[])
 {
-//    try
-//    {
-//        tcp::resolver resolver(io_service);
-//        tcp::resolver::query query("127.0.0.1", "3310");
-//        tcp::resolver::iterator iterator = resolver.resolve(query);
-//
-//        kk_client c(io_service, iterator);
-//
-//        std::thread t_socket([&] { io_service.run(); c.close();});
-//        std::thread t_sender([&] { while(c.get_connection_state() != connection_failed ){c.menu();};});
-//
-//        t_socket.join();
-//        t_sender.join();
-//    }
-//    catch (std::exception& e)
-//    {
-//        std::cerr << "Exception: " << e.what() << "\n";
-//    }
-
-
-//    Q_INIT_RESOURCE(textedit);
-
+    Q_INIT_RESOURCE(client);
     QApplication a(argc, argv);
+    kk_client client(QUrl(QStringLiteral("wss://localhost:3310")));
+
+
     QCoreApplication::setOrganizationName("KlapKlapTeam");
     QCoreApplication::setApplicationName("KlapKlap");
     QCoreApplication::setApplicationVersion(QT_VERSION_STR);
@@ -57,7 +33,7 @@ int main(int argc, char* argv[])
     parser.addPositionalArgument("file", "The file to open.");
     parser.process(a);
 
-    QMainWindow mw;
+    TextEdit mw;
 
     const QRect availableGeometry = QApplication::desktop()->availableGeometry(&mw);
     mw.resize(availableGeometry.width() / 2, (availableGeometry.height() * 2) / 3);
@@ -68,5 +44,6 @@ int main(int argc, char* argv[])
 //        mw.fileNew();
 
     mw.show();
+    Q_UNUSED(client);
     return a.exec();
 }
