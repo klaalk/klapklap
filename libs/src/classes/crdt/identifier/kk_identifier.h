b0483fc7a8320848f04e2fc5ed23dccb2f210e83
Klaus Cuko <klaus.cuko@gmail.com>
2019-06-09 13:20:52 +0200

    fix crdt local insert

--- libs/src/classes/crdt/identifier/kk_identifier.h
+++ libs/src/classes/crdt/identifier/kk_identifier.h
@@ -1,31 +1,25 @@
 //
 // Created by Alberto Bruno on 2019-05-15.
 //
+#ifndef KK_IDENTIFIER_H
+#define KK_IDENTIFIER_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <iostream>
-
-
-#ifndef KK_IDENTIFIER_H
-#define KK_IDENTIFIER_H
+#include <memory>
 
 using std::string;
-
 class kk_identifier {
     int digit; // cifra della posizione
     string siteid; //univoco del client, serve per calcolare posizione in caso di stessi digit
-
 public:
     kk_identifier(int digit, string siteid); //costruttore
-    int compare_to(
-            const kk_identifier &other); // torna -1 se questo < l'altro, 1 se questo > l'altro e lo fa confrontando prima il digit e poi se il digit è uguale, il siteId
+    int compare_to(const kk_identifier &other); // torna -1 se questo < l'altro, 1 se questo > l'altro e lo fa confrontando prima il digit e poi se il digit è uguale, il siteId
     int get_digit();
-
     void set_digit(int digit);
-
     string get_siteid();
 };
 
-
+typedef std::shared_ptr<kk_identifier> kk_identifier_ptr;
 #endif //KK_IDENTIFIER_H
--- libs/src/classes/crdt/pos/kk_pos.h
+++ libs/src/classes/crdt/pos/kk_pos.h
@@ -6,19 +6,17 @@
 #define KK_POS_H
 
 #include "../../../kklibexport.h"
+#include <memory>
 
 class kk_pos {
     int ch;
     int line;
 public:
     kk_pos();
-
     kk_pos(int line, int ch);
-
     int get_line();
-
     int get_ch();
 };
 
-
+typedef std::shared_ptr<kk_pos> kk_pos_ptr;
 #endif //KK_POS_H
