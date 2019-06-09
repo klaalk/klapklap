e4853f3513f8a5ff06652dcd22026f12b053c108
Michele Luigi Greco <grecomichele96@gmail.com>
2019-06-09 19:25:25 +0200

    filesys

---
+++ server/src/classes/server/filesys/kk_filesys.cpp
@@ -0,0 +1,5 @@
+//
+// Created by michele on 09/06/2019.
+//
+
+#include "kk_filesys.h"
---
+++ server/src/classes/server/filesys/kk_filesys.h
@@ -0,0 +1,48 @@
+//
+// Created by michele on 09/06/2019.
+//
+
+#ifndef KK_FILESYS_H
+#define KK_FILESYS_H
+
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <QSharedPointer>
+#include <QWebSocket>
+#include <QFile>
+#define FILE_PATH "./filefolder/"
+
+
+class kk_filesys {
+public:
+    kk_filesys();
+    ~kk_filesys();
+
+    void filesysOpenFile(QString username, QString filename, QString path){
+        QString command="touch "+ path ;
+        QFile file(filename);
+        QStringList list;
+
+        if(files_tree.find(file)==files_tree.end()){
+            system(qPrintable(command));
+            list.push_back(username);
+            files_tree.insert(file,list);
+
+
+        }else {
+            files_tree.find(file)->push_back(username);
+        }
+
+    }
+
+
+
+private:
+    QMap <QFile,QStringList>files_tree; //chiave: puntatore al file, value: lista degli utenti conessi a quel file
+
+};
+typedef QSharedPointer<kk_filesys> kk_filesys_ptr;
+#endif //KK_FILESYS_H
