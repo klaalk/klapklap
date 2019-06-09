d221c01bee0922179543508a2d81f3221dcab767
Klaus Cuko <klaus.cuko@gmail.com>
2019-06-09 13:20:52 +0200

    fix crdt local insert

--- libs/src/classes/crdt/char/kk_char.h
+++ libs/src/classes/crdt/char/kk_char.h
@@ -1,42 +1,36 @@
 //
 // Created by Alberto Bruno on 2019-05-15.
 //
+#ifndef KK_CHAR_H
+#define KK_CHAR_H
+
 #include "../identifier/kk_identifier.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 #include <math.h>
 #include <iostream>
+#include <algorithm>
+#include <memory>
 
 using std::vector;
 using std::string;
 
-
-#ifndef KK_CHAR_H
-#define KK_CHAR_H
-
-
 class kk_char {
-    vector<kk_identifier> position; // array di identifier, serve per dare come posizioni univoche numeri anche con la virgola (es 0,5 -> [identifier1.digit=0][identifier2.digit=5])
+    vector<kk_identifier_ptr> position; // array di identifier, serve per dare come posizioni univoche numeri anche con la virgola (es 0,5 -> [identifier1.digit=0][identifier2.digit=5])
     string siteId; //univoco per il client
     char value; //valore (es 'a')
-
-
 public:
-    kk_char();
-
     kk_char(char value, string siteId);//costruttore
+
     char get_value();
-
     int compare_to(const kk_char &other); //torna: 1 - questa > l'altra, -1 l'altra > questa, 0 se no
-    void push_identifier(
-            kk_identifier x); //mette l'identifier nuovo come ultimo elemento del vettore posizione(aggiunge una cifra)
-    void insert_identifier(vector<kk_identifier>::iterator it, kk_identifier id);
-
-    void insert_position(vector<kk_identifier> position);
-
-    vector<kk_identifier> get_position();
-
+    void push_identifier(kk_identifier_ptr x); //mette l'identifier nuovo come ultimo elemento del vettore posizione(aggiunge una cifra)
+    void insert_identifier(vector<kk_identifier_ptr>::iterator it, kk_identifier_ptr id);
+    void insert_position(vector<kk_identifier_ptr> position);
+    vector<kk_identifier_ptr> get_position();
 };
+
+typedef std::shared_ptr<kk_char> kk_char_ptr;
 
 #endif //KK_CHAR_H
--- libs/src/classes/crdt/kk_crdt.h
+++ libs/src/classes/crdt/kk_crdt.h
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <vector>
+#include <algorithm>
 #include <list>
 
 #include "char/kk_char.h"
@@ -27,37 +28,35 @@ enum strategy {
 
 class kk_crdt {
 public:
-    vector<list<kk_char>> text;
+    vector<list<kk_char_ptr>> text;
     string siteid;
     int boundary;
     strategy _strategy;
     vector<strategy> strategy_cache;
     int base;
 
-    vector<kk_identifier> slice(vector<kk_identifier> const &v, int i);
-
     kk_crdt(string siteid, strategy strategy); //costruttore
     void local_insert(char val, kk_pos pos);
 
-    kk_char generate_Char(char val, kk_pos pos); //genera la Char partendo dal valore e dalla posizione nel local text
+    kk_char_ptr generate_Char(char val, kk_pos pos); //genera la Char partendo dal valore e dalla posizione nel local text
 
-    vector<kk_identifier> find_position_before(kk_pos pos); //trova la position della Char immediatamente prima di quella passata
+    vector<kk_identifier_ptr> find_position_before(kk_pos pos); //trova la position della Char immediatamente prima di quella passata
 
-    vector<kk_identifier> find_position_after(kk_pos pos);//trova la position della Char immediatamente dopo di quella passata
+    vector<kk_identifier_ptr> find_position_after(kk_pos pos);//trova la position della Char immediatamente dopo di quella passata
 
-    vector<kk_identifier> generate_position_between(vector<kk_identifier> position_1, vector<kk_identifier> position_2,
-                                                    vector<kk_identifier> *new_position, int livello); //partendo dalle position di due Char(adiacenti) genera la posizione della Char
+    vector<kk_identifier_ptr> generate_position_between(vector<kk_identifier_ptr> position_1, vector<kk_identifier_ptr> position_2,
+                                                    vector<kk_identifier_ptr> *new_position, int livello); //partendo dalle position di due Char(adiacenti) genera la posizione della Char
 
     strategy find_strategy(int level);//trova la strategia migliore per assegnare un identifier alla position della nuova Char
 
-    int generate_identifier_between(int min, int max, strategy _strategy); //dati due identifier ne genera uno nuovo da mettere nella position della nuova Char usando la strategia opportuna
+    double generate_identifier_between(int min, int max, strategy _strategy); //dati due identifier ne genera uno nuovo da mettere nella position della nuova Char usando la strategia opportuna
 
-    //    void handle_remote_insert(kk_char _char);
+//    void handle_remote_insert(kk_char _char);
 //    kk_pos find_insert_position(kk_char _char);
 //    int is_empty();
 //    kk_pos find_end_position (kk_char last_char,  list<kk_char> last_line, int total_lines);
 //    int find_insert_index_in_line(kk_char _char, list<kk_char> line);
-    void insert_char(kk_char _char, kk_pos pos);
+    void insert_char(kk_char_ptr _char, kk_pos pos);
 //    void local_delete(kk_pos start_pos, kk_pos end_pos);
 //    list<kk_char> delete_multiple_lines(kk_pos start_pos, kk_pos end_pos);
 //    list<kk_char> delete_single_line(kk_pos start_pos, kk_pos end_pos);
@@ -66,6 +65,7 @@ public:
 //    void remote_delete(kk_char _Char);
 //    kk_pos find_pos (kk_char _Char);
 //    int find_index_in_line(kk_char _Char, int line);
+    vector<kk_identifier_ptr> slice(vector<kk_identifier_ptr> const &v, int i);
     void print();
 };
 
