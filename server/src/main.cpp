31299d62fc0a4f03b6d6a1633e1192579a8beaa5
Klaus Cuko <klaus.cuko@gmail.com>
2019-06-12 21:28:29 +0200

    fix merge

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -18,19 +18,19 @@ int main(int argc, char *argv[]) {
 
 
     QApplication a(argc, argv);
-//    kk_server server(3002);
-//    Q_UNUSED(server);
+    kk_server server(3002);
+    Q_UNUSED(server);
 
-    kk_db_ptr temporary = kk_db_ptr(new kk_db());
-    kk_filesys filesys(temporary);
+//    kk_db_ptr temporary = kk_db_ptr(new kk_db());
+//    kk_filesys filesys(temporary);
 
-//    for (int i =0;i<5;i++) {
-//        filesys.kk_CreateFile("grecomichele96","prova.txt");
-//    }
+////    for (int i =0;i<5;i++) {
+////        filesys.kk_CreateFile("grecomichele96","prova.txt");
+////    }
 
-    filesys.kk_OpenFile("klaus.cuko","2qmWqBlQ9wQj99nsQzld_AwIJdRfcCrvykd9GnFiZIW5bTg==_prova.txt");
+//    filesys.kk_OpenFile("klaus.cuko","2qmWqBlQ9wQj99nsQzld_AwIJdRfcCrvykd9GnFiZIW5bTg==_prova.txt");
 
-    qDebug() << "done";
+//    qDebug() << "done";
 
     return a.exec();
 }
